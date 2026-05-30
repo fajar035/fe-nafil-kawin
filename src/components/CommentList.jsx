f966fa0a22be540e9a3d4b232d69601e3db1aa1e
fajar035 <ikehikeh151@gmail.com>
2026-05-30 19:46:21 +0700

    fix handle no data

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -16,6 +16,7 @@ const formatDate = (isoString) => {
 // Output: 30 Mei 2026, 14.10.00
 
 function CommentList({ data }) {
+  //   const data = [];
   return (
     <motion.div
       initial={{
@@ -29,50 +30,58 @@ function CommentList({ data }) {
       viewport={{
         once: true,
       }}
-      className="bg-white rounded-[30px] shadow-md p-3 mx-5 mb-10 mt-5"
+      className="bg-white w-full rounded-[30px] shadow-md p-3 mx-5 mb-10 mt-5"
     >
       <h2 className="text-2xl font-bold text-[#6b2222] mb-6 mt-3 ml-3">
         Daftar Ucapan
       </h2>
 
       <div className="pr-2 space-y-4 overflow-y-auto max-h-125">
-        {data?.map((item, index) => (
-          <motion.div
-            key={item.id}
-            initial={{
-              opacity: 0,
-              x: -20,
-            }}
-            whileInView={{
-              opacity: 1,
-              x: 0,
-            }}
-            transition={{
-              delay: index * 0.1,
-            }}
-            className="bg-gray-50 rounded-[20px] p-5 border border-gray-100"
-          >
-            <div className="flex items-start justify-between">
-              <div>
-                <h3 className="text-xl font-bold text-gray-800">{item.name}</h3>
+        {data?.length === 0 ? (
+          <div className="bg-gray-50 w-full rounded-[20px] p-5 border border-gray-100 text-center">
+            <p>No data</p>
+          </div>
+        ) : (
+          data?.map((item, index) => (
+            <motion.div
+              key={item.id}
+              initial={{
+                opacity: 0,
+                x: -20,
+              }}
+              whileInView={{
+                opacity: 1,
+                x: 0,
+              }}
+              transition={{
+                delay: index * 0.1,
+              }}
+              className="bg-gray-50 rounded-[20px] p-5 border border-gray-100"
+            >
+              <div className="flex items-start justify-between">
+                <div>
+                  <h3 className="text-xl font-bold text-gray-800">
+                    {item.name}
+                  </h3>
 
-                <p className="mt-1 text-sm text-gray-400">
-                  {formatDate(item.created_at)}
-                </p>
+                  <p className="mt-1 text-sm text-gray-400">
+                    {formatDate(item.created_at)}
+                  </p>
+                </div>
+
+                <span
+                  className={`font-semibold text-sm ${
+                    item.available === 1 ? "text-green-500" : "text-red-500"
+                  }`}
+                >
+                  {item.available === 1 ? "✓ Hadir" : "✗ Tidak Hadir"}
+                </span>
               </div>
 
-              <span
-                className={`font-semibold text-sm ${
-                  item.available === 1 ? "text-green-500" : "text-red-500"
-                }`}
-              >
-                {item.available === 1 ? "✓ Hadir" : "✗ Tidak Hadir"}
-              </span>
-            </div>
-
-            <p className="mt-4 text-gray-700">{item.comment}</p>
-          </motion.div>
-        ))}
+              <p className="mt-4 text-gray-700">{item.comment}</p>
+            </motion.div>
+          ))
+        )}
       </div>
     </motion.div>
   );
